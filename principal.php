1594217e66d178e577d43b0a3f4dd79f6f71a5d2
thiago-natale <thiagohenrique.natale@gmail.com>
2024-09-02 17:21:31 -0300

    Atualização arquivo CSS

--- principal.php
+++ principal.php
@@ -13,14 +13,16 @@ session_start()
 
 <body>
     <header class="top-bar">
-        <div>
-            <h1>Olá <?php echo $_SESSION["nome"]; ?> </h1>
-        </div>
-        <button value="sair" id="sair">sair</button>
+        <h1>Olá
+            <?php echo $_SESSION["nome"]; ?>
+        </h1>
+        <button value="sair" id="sair">Sair</button>
     </header>
     <main>
         <section>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magnam, molestiae. Tempore maiores quos, amet, similique incidunt nostrum harum ad laudantium saepe veniam consequuntur excepturi. Id perferendis et corporis quo illum!</p>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magnam, molestiae. Tempore maiores quos, amet,
+                similique incidunt nostrum harum ad laudantium saepe veniam consequuntur excepturi. Id perferendis et
+                corporis quo illum!</p>
         </section>
     </main>
 </body>
